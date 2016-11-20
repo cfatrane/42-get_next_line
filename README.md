8adb4ce86a78326b0d15e2b794a12e5ea32dfe63
cfatrane <cfatrane@student.42.fr>
2016-11-20 13:09:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 42_get_next_line
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfatrane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/17 18:14:50 by cfatrane          #+#    #+#             */
+/*   Updated: 2016/11/17 18:20:08 by cfatrane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFF_SIZE
+
+int	get_next_line(const int fd, char **line);
+
+#endif
