1e84ee427df2f5347040c1dd8b1b2ed9a46e4b68
cfatrane <cfatrane@student.42.fr>
2016-11-23 11:33:51 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 13:51:49 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/11/22 19:40:05 by cfatrane         ###   ########.fr       */
+/*   Updated: 2016/11/23 11:33:20 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ int	get_next_line(const int fd, char **line)
 	ret = read(fd, buf, BUFF_SIZE);
 	if (fd == -1 || ret == -1)
 		return (-1);
-	ft_strrchr(*line , '\n');
+	while (!(ft_strchr(*line , '\n')))
+	{
+		buf[ret] = '\0';
+
+	}
 	return (0);
 }
