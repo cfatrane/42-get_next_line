9ce7acf513d47dd17b9ba91f85dce87af6e21a70
cfatrane <cfatrane@student.42.fr>
2016-11-27 14:59:10 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,60 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 13:51:49 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/11/26 17:41:12 by cfatrane         ###   ########.fr       */
+/*   Updated: 2016/11/27 14:52:02 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+/*int	multifd()
+{
+
+}*/
+
+int	gnl(char **line, char *save)
+{
+	char *eol;
+
+	eol = ft_strchr(save, '\n');
+	if (eol != NULL)
+	{
+		*eol = '\0';
+		*line = ft_strdup(save);
+		ft_memmove(save, &eol[1], ft_strlen(&eol[1]) + 1);
+		return (1);
+	}
+	if (ft_strlen(save) > 0)
+	{
+		*line = ft_strdup(save);
+		*save = '\0';
+		return (1);
+	}
+	return (0);
+}
 
 int	get_next_line(const int fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
-	static char	*save;
-	char		*join;
+	static char	*save = NULL;
+	char		*tmp;
 	int			ret;
 
-	ret = read(fd, buf, BUFF_SIZE);
-	if (fd < 0 || *line == NULL || ret == -1)
+	if (fd == -1 || line == NULL || BUFF_SIZE <= 0)
 		return (-1);
-	save = ft_strnew(1);
-	while (!(ft_strchr(*line, '\n')))
+	if (save == NULL)
+		save = ft_strnew(0);
+	while (!(ft_strchr(save, '\n')))
 	{
 		ret = read(fd, buf, BUFF_SIZE);
-		buf[ret] = '\0';
+		if (ret == -1)
+			return (-1);
 		if (ret == 0)
-			return (0);
-		join = ft_strjoin(save, buf);
-		free (save);
+			break ;
+		buf[ret] = '\0';
+		tmp = ft_strjoin(save, buf);
+		free(save);
+		save = tmp;
 	}
-	return (0);
+	return (gnl(line, save));
 }
