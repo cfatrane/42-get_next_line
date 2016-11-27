8db9ae71e2d316fa588ffbad70753f33b095bed1
cfatrane <cfatrane@student.42.fr>
2016-11-27 15:07:52 +0100

    get_next_line

--- README.md
+++
@@ -1,3 +0,0 @@
-# 42_get_next_line
-Ce projet a pour but de nous faire coder une fonction qui renvoit une ligne
-terminée par un retour à la ligne lue depuis un descripteur de fichier
--- main.c
+++ main.c
@@ -1,23 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/26 17:34:20 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/11/26 17:35:32 by cfatrane         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "get_next_line.h"
 
-int	main(int argc,char **argv)
+/*
+** 2 lines with 8 chars with Line Feed
+*/
+
+int				main(void)
 {
+	char		*line;
+	int			fd;
+	int			ret;
+	int			count_lines;
+	char		*filename;
+	int			errors;
 
-
-
-
-
+	filename = "gnl1_2.txt";
+	fd = open(filename, O_RDONLY);
+	if (fd > 2)
+	{
+		count_lines = 0;
+		errors = 0;
+		line = NULL;
+		while ((ret = get_next_line(fd, &line)) > 0)
+		{
+			if (count_lines == 0 && strcmp(line, "1234567") != 0)
+				errors++;
+			if (count_lines == 1 && strcmp(line, "abcdefg") != 0)
+				errors++;
+			count_lines++;
+			if (count_lines > 50)
+				break ;
+		}
+		close(fd);
+		if (count_lines != 2)
+			printf("-> must have returned '1' twice instead of %d time(s)\n", count_lines);
+		if (errors > 0)
+			printf("-> must have read \"1234567\" and \"abcdefg\"\n");
+		if (count_lines == 2 && errors == 0)
+			printf("OK\n");
+	}
+	else
+		printf("An error occured while opening file %s\n", filename);
 	return (0);
 }
